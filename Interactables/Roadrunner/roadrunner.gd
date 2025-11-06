524f8dc365388d3b52910d3115a355e2a891b2b8
breadmaster <ethanchereau8@gmail.com>
2025-11-06 11:00:10 -0800

    Reduce roadrunner flee & wander speeds

--- Interactables/Roadrunner/roadrunner.gd
+++ Interactables/Roadrunner/roadrunner.gd
@@ -3,8 +3,8 @@ extends CharacterBody3D
 
 @export var player: Node3D
 
-const WANDER_SPEED: float = 4.0
-const FLEE_SPEED: float = 6.0
+const WANDER_SPEED: float = 2.0
+const FLEE_SPEED: float = 4.5
 const FLEE_DISTANCE: float = 5.0
 const SAFE_DISTANCE: float = 20.0
 const WANDER_INTERVAL: float = 3.0
